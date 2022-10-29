bc03f26ade48f768ce93b8997873a617bdd28efb
lelinrashed <lelin.rashed784@gmail.com>
2022-10-29 09:11:57 +0600

    array

---
+++ array/basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A[]={3, 4, 5, 7};
+
+    cout<<sizeof(A)<<endl;
+    cout<<A[1]<<endl;
+    printf("%d\n", A[2]);
+
+    return 0;
+}
--- intro/basicCalculation.c
+++ intro/basicCalculation.c
@@ -5,9 +5,6 @@ int main()
 
     printf("%d\n", 5 + 10);
     printf("%d\n", 5 * 10);
-    printf("%d\n", 50 / 10);
-    printf("%d", 50 % 7);
-
 
     return 0;
 }
